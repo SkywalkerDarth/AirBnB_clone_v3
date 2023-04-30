a9aaf5f98497dec2d73e95543998eaf14b0443c4
SkywalkerDarth <danteuche@gmail.com>
2023-04-30 12:17:17 -0700

    task

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -11,3 +11,13 @@ def get_status():
     """creates a route on app_views that returns JSON"""
     return jsonify({'status': 'OK'})
 
+@app_views.route('/stats', methods=['GET'], strict_slashes=False)
+def stats():
+    """ Create an endpoint that retrieves the
+    number of each objects by type: """
+    return jsonify(amenities=storage.count("Amenity"),
+                   cities=storage.count("City"),
+                   places=storage.count("Place"),
+                   reviews=storage.count("Review"),
+                   states=storage.count("State"),
+                   users=storage.count("User"))
