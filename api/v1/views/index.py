6a68a6344f325181427aca069d47a9dfdffe9d09
SkywalkerDarth <danteuche@gmail.com>
2023-04-30 11:16:02 -0700

    fix

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 """
-view.py file
+index.py
 """
 from models.state import State
 from flask import jsonify, Blueprint
@@ -13,7 +13,7 @@ def get_status():
     """creates a route on app_views that returns JSON"""
     return jsonify({'status': 'OK'})
 
-@app_views.route('/status', methods=['GET'], strict_slashes=False)
+@app_views.route('/stats', methods=['GET'], strict_slashes=False)
 def object_status():
     """Creates an endpoint, retrieves number of each object by type"""
     objects = {"amenities": 'Amenity', "cities": 'City',
